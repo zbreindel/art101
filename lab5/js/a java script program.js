e47c9003478f327aa54a4a31a581820a552c8cf9
zbreindel <zbreinde@ucsc.edu>
2025-05-01 19:50:01 -0700

    1
    1

---
+++ lab5/js/a java script program.js
@@ -0,0 +1,21 @@
+/**
+ * Author: Zachary Breindel
+ * Created: 24 April 2025
+ * Course: ART101
+ */
+// Define variables
+make = "Ford";
+model = "Focus";
+color = "Yellow";
+year = 2012;
+
+//calculate
+age = 2025 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color:" + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years <br>");
+
